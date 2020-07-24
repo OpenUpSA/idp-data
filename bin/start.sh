824d1cac920753b60047053da71ba137da3b3b3c
JD Bothma <jbothma@gmail.com>
2020-07-24 17:21:31 +0200

    Fix start script

--- bin/start.sh
+++ bin/start.sh
@@ -6,4 +6,4 @@ set -o nounset
 
 
 python manage.py collectstatic --noinput
-gunicorn  --worker-class gevent zanews.wsgi:application --log-file - --bind 0.0.0.0:5000
+gunicorn  --worker-class gevent idp_data.wsgi:application --log-file - --bind 0.0.0.0:5000
