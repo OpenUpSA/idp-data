b7bc2f6ca7ebbce2e5682922c1648409d2659faa
emre2038 <emreyildirim2038@gmail.com>
2020-08-26 08:56:06 -0400

    removed requirements.txt, removed unnecessary ckeditor extensions

--- idp_data/settings.py
+++ idp_data/settings.py
@@ -181,7 +181,6 @@ if TAG_MANAGER_ENABLED:
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
-CKEDITOR_IMAGE_BACKEND = "pillow"
 
 CKEDITOR_CONFIGS = {
     'default': {
