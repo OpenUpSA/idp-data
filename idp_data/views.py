39a59b6cec67a74c2f430c85bdba62d9d906ffd4
Paul Watson <github.com@paulmwatson.com>
2021-04-09 11:40:20 +0200

    Added recaptcha_score float to event_submissions and method to get the value from Google

--- idp_data/views.py
+++ idp_data/views.py
@@ -1,3 +1,5 @@
+import requests
+from os import environ
 from django.views import generic
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,6 +9,7 @@ from django.shortcuts import get_object_or_404
 from django.db.models import Case, CharField, Value, When
 from rest_framework import status
 from rest_framework.parsers import JSONParser
+
 
 class Index(generic.TemplateView):
     template_name = "index.html"
@@ -42,6 +45,20 @@ def geography_detail(request):
 def event_submissions(request):
     if request.POST:
         event_submission_data = request.data
+
+        #Todo: Refactor into method
+        #Note: In future the recaptcha score will be used to determine whether
+        #      to save and email the event submission or not
+        if token := event_submission_data['recaptcha_token']:
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            payload = {'secret': environ.get('RECAPTCHA_SECRET'),'response': token}
+            response = requests.post(url, data = payload)
+            if response.status_code == 200:
+                data = response.json()
+                event_submission_data._mutable = True
+                event_submission_data['recaptcha_score'] = data['score']
+                event_submission_data._mutable = False
+
         event_submission_serializer = EventSubmissionSerializer(data=event_submission_data)
 
         if event_submission_serializer.is_valid():
@@ -55,7 +72,3 @@ def event_submissions(request):
         return Response(serializer.data)
     else:
         return Response("Unsupoorted method")
-
-#TODO: Do not expose this in production
-#@api_view(['GET'])
-#def event_submissions_details():
