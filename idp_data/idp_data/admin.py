16c04857df5ff305f533465c62ea806c484b5d38
Paul Watson <github.com@paulmwatson.com>
2021-04-29 15:37:25 +0200

    Used list_display to add columns and date_hierarchy to add date filtering to event submissions in admin

--- idp_data/idp_data/admin.py
+++ idp_data/idp_data/admin.py
@@ -1,20 +1,39 @@
 from django.contrib import admin
 from .models import Event, Category, Municipality, EventAction, MunicipalityHostname, EventSubmission
 
+
 class EventActionInline(admin.StackedInline):
     model = EventAction
+
 
 class EventAdmin(admin.ModelAdmin):
     inlines = [EventActionInline]
 
+
 class MunicipalityHostnameInline(admin.StackedInline):
     model = MunicipalityHostname
+
 
 class MunicipalityAdmin(admin.ModelAdmin):
     inlines = [MunicipalityHostnameInline]
 
 
+class EventSubmissionAdmin(admin.ModelAdmin):
+    model = EventSubmission
+    date_hierarchy = 'submitted'
+    list_display = ('submitted',
+                    'event',
+                    'submission_issue',
+                    'submitter_town',
+                    'submitter_name',
+                    'submitter_contact',
+                    'event_muni')
+
+    def event_muni(self, obj):
+        return obj.event.muni.name
+
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(Category)
-admin.site.register(EventSubmission)
+admin.site.register(EventSubmission, EventSubmissionAdmin)
 admin.site.register(Municipality, MunicipalityAdmin)
