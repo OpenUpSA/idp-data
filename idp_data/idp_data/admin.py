d2775ff9af1e7f4475fb7db0434f9c5072cbd2db
Paul Watson <github.com@paulmwatson.com>
2021-04-30 11:30:28 +0200

    Added chart to event submissiosn with filtering

--- idp_data/idp_data/admin.py
+++ idp_data/idp_data/admin.py
@@ -1,4 +1,9 @@
+import json
 from django.contrib import admin
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models.functions import TruncDay
+from django.db.models import Count
+
 from .models import Event, Category, Municipality, EventAction, MunicipalityHostname, EventSubmission
 
 
@@ -29,6 +34,30 @@ class EventSubmissionAdmin(admin.ModelAdmin):
                     'submitter_contact',
                     'event_muni')
     list_filter = ('submitter_town', 'submission_issue', 'event')
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context)
+        queryset = response.context_data["cl"].queryset
+        chart_data = (queryset.annotate(date=TruncDay("submitted"))
+                      .values("date")
+                      .annotate(y=Count("id"))
+                      .order_by("-date"))
+        
+        all_chart_data = (EventSubmission.objects.annotate(date=TruncDay("submitted"))
+            .values("date")
+            .annotate(y=Count("id"))
+            .order_by("-date"))
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        all_as_json = json.dumps(list(all_chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json, "all_chart_data": all_as_json}
+
+        #as_json[0] = {"date": "2021-03-1T00:00:00Z", "y": 0}
+        print(as_json)
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
 
     def event_muni(self, obj):
         return obj.event.muni.name
